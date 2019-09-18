47e42bf2ddb663bf0788a7ebfb636629afa05414
irissiri7 <48020183+irissiri7@users.noreply.github.com>
2019-09-18 21:56:33 +0200

    Learning Git nr 2....

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("HEJ");
+            Console.WriteLine("HEJDÅ");
             try
             {
                 PhotoShopLib.ImageEditor.MakeBlurr(args[0]);
