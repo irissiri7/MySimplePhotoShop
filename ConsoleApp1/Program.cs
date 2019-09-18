06476f4226707f9b382190d8b273fbeb1d2ece4a
Lydia Nilsson <lydia.nilsson@gmail.com>
2019-09-18 21:53:53 +0200

    learning Git....

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("HEJ");
             try
             {
                 PhotoShopLib.ImageEditor.MakeBlurr(args[0]);
